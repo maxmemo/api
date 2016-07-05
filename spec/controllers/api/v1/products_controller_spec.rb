dca326bd52ef239edb15856ecb22c7f17ef0bc49
Maxime Riard <maxime.riard@gmail.com>
2016-07-05 09:54:27 +0200

    adds product serializer!

--- spec/controllers/api/v1/products_controller_spec.rb
+++ spec/controllers/api/v1/products_controller_spec.rb
@@ -8,7 +8,7 @@ describe Api::V1::ProductsController do
     end
 
     it "returns the information about a reporter on a hash" do
-      product_response = json_response
+      product_response = json_response[:product]
       expect(product_response[:title]).to eql @product.title
     end
 
@@ -23,7 +23,7 @@ describe Api::V1::ProductsController do
 
     it "returns 4 records from the database" do
       products_response = json_response
-      expect(products_response.length).to eq(4)
+      expect(products_response[:products]).to have(4).items
     end
 
     it { should respond_with 200 }
@@ -39,7 +39,7 @@ describe Api::V1::ProductsController do
       end
 
       it "renders the json representation for the product record just created" do
-        product_response = json_response
+        product_response = json_response[:product]
         expect(product_response[:title]).to eql @product_attributes[:title]
       end
 
@@ -82,7 +82,7 @@ describe Api::V1::ProductsController do
       end
 
       it "renders the json representation for the updated user" do
-        product_response = json_response
+        product_response = json_response[:product]
         expect(product_response[:title]).to eql "An expensive TV"
       end
 
