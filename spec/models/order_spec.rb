5cbf148ae245a81381d303c6b54047e51c8760c2
Maxime Riard <maxime.riard@gmail.com>
2016-07-05 12:17:25 +0200

    allow product quantity

--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -25,6 +25,19 @@ describe Order do
       @order = FactoryGirl.build :order, product_ids: [product_1.id, product_2.id]
     end
 
+    describe "#build_placements_with_product_ids_and_quantities" do
+      before(:each) do
+        product_1 = FactoryGirl.create :product, price: 100, quantity: 5
+        product_2 = FactoryGirl.create :product, price: 85, quantity: 10
+
+        @product_ids_and_quantities = [[product_1.id, 2], [product_2.id, 3]]
+      end
+
+      it "builds 2 placements for the order" do
+        expect{order.build_placements_with_product_ids_and_quantities(@product_ids_and_quantities)}.to change{order.placements.size}.from(0).to(2)
+      end
+    end
+
     it "returns the total amount to pay for the products" do
       expect{@order.set_total!}.to change{@order.total}.from(0).to(185)
     end
